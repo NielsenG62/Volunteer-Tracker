f74418610514b87895db2cf70972d599240689dc
Gabe Nielsen <gabenielsen7@gmail.com>
2022-05-20 09:46:54 -0700

    add code to pass Volunteer test 3

--- lib/volunteer.rb
+++ lib/volunteer.rb
@@ -16,4 +16,12 @@ class Volunteer
     @project_id
   end
 
+  def ==(volunteer_to_compare)
+    if volunteer_to_compare != nil
+      (self.name == volunteer_to_compare.name) && (self.project_id == volunteer_to_compare.project_id)
+    else
+      false
+    end
+  end
+
 end
