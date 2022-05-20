628e3a3282f747cd10584db895cc01f662b4428a
Gabe Nielsen <gabenielsen7@gmail.com>
2022-05-20 09:41:50 -0700

    add code to pass Volunteer test 1

---
+++ lib/volunteer.rb
@@ -0,0 +1,14 @@
+class Volunteer
+  attr_reader :id
+  attr_accessor :name
+
+  def initialize(attributes)
+    @name = attributes.fetch(:name)
+    @id = attributes.fetch(:id)
+  end
+
+  def name
+    @name
+  end
+
+end
