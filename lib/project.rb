b12f9ebe92896f1dd2bd9e0692f516bddead10b5
Gabe Nielsen <gabenielsen7@gmail.com>
2022-05-20 09:23:47 -0700

    change database column names from name => title for consistent naming conventions

--- lib/project.rb
+++ lib/project.rb
@@ -1,6 +1,6 @@
 class Project
   attr_reader :id
-  attr_accessor :name
+  attr_accessor :title
 
   def initialize(attributes)
     @title = attributes.fetch(:title)
@@ -16,19 +16,19 @@ class Project
   end
 
   def save
-    result = DB.exec("INSERT INTO projects (name) VALUES ('#{@title}') RETURNING id;")
+    result = DB.exec("INSERT INTO projects (title) VALUES ('#{@title}') RETURNING id;")
     @id = result.first.fetch("id").to_i
   end
 
   def ==(project_to_compare)
-    self.name == project_to_compare.name
+    self.title == project_to_compare.title
   end
 
   def self.all
     returned_projects = DB.exec("SELECT * FROM projects;")
     projects = []
     returned_projects.each do |project|
-      title = project.fetch("name")
+      title = project.fetch("title")
       id = project.fetch("id").to_i
       projects.push(Project.new({:title => title, :id => id}))
     end
@@ -37,7 +37,7 @@ class Project
 
   def self.find(id)
     project = DB.exec("SELECT * FROM projects WHERE id = #{id};").first
-    title = project.fetch("name")
+    title = project.fetch("title")
     id = project.fetch("id")
     Project.new ({:title => title, :id => id})
   end
