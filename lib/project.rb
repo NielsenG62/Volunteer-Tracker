73c0b118394e7d16470834c744d094e32dded429
Gabe Nielsen <gabenielsen7@gmail.com>
2022-05-20 08:37:04 -0700

    add code to pass first project test

---
+++ lib/project.rb
@@ -0,0 +1,15 @@
+class Project
+  attr_reader :id
+  attr_accessor :name
+
+  def initialize(attributes)
+    @title = attributes.fetch(:title)
+    @id = attributes.fetch(:id)
+  end
+
+  def title
+    @title
+  end
+
+end
+
